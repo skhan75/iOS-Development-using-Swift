9c949eb4c31195bd66f6cdcea48343596f69c159
skhan75 <samiahmadkhan@Samis-MacBook-Pro.local>
2016-06-28 05:36:02 -0500

    Added bridge header and .db file

---
+++ mp/03/Hackathon/Hackathon/bridging-header.h
@@ -0,0 +1,14 @@
+//
+//  bridging-header.h
+//  SQLiteDemo
+//
+//  Created by Michael Lee on 6/13/16.
+//  Copyright © 2016 Michael Lee. All rights reserved.
+//
+
+#ifndef bridging_header_h
+#define bridging_header_h
+
+#import <sqlite3.h>
+
+#endif /* bridging_header_h */
