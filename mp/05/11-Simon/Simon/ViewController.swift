4e5cef2f1d191865a9ada02b2fb458c53eff60f0
skhan75 <samiahmadkhan@Samis-MacBook-Pro.local>
2016-07-01 06:49:19 -0500

    Simon project started

---
+++ mp/05/11-Simon/Simon/ViewController.swift
@@ -0,0 +1,81 @@
+//
+//  ViewController.swift
+//  Simon
+//
+//  Created by Michael Lee on 6/27/16.
+//  Copyright © 2016 Michael Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  @IBOutlet weak var messageLabel: UILabel!
+  @IBOutlet var buttons: [UIButton]!
+  var sequence = [3, 2, 0, 1]
+  var guessidx = 0
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    messageLabel.text = "Double tap to start!"
+  }
+  
+  func flashSequence() {
+    let queue = dispatch_get_global_queue(QOS_CLASS_USER_INITIATED, 0)
+    self.messageLabel.text = "Watch carefully..."
+    dispatch_async(queue) {
+      for btnidx in self.sequence {
+        dispatch_async(dispatch_get_main_queue()) {
+          self.buttons[btnidx].highlighted = true
+        }
+        NSThread.sleepForTimeInterval(1.0)
+        dispatch_async(dispatch_get_main_queue()) {
+          self.buttons[btnidx].highlighted = false
+        }
+      }
+      dispatch_async(dispatch_get_main_queue()) {
+        self.messageLabel.text = "Tap out the sequence!"
+      }
+    }
+  }
+  
+  @IBAction func buttonTapped(sender: UIButton) {
+    print(guessidx)
+    if buttons[sequence[guessidx]] == sender {
+      messageLabel.text = "Good guess"
+      guessidx += 1
+      if guessidx == buttons.count {
+        messageLabel.text = "You win (double tap to restart)"
+        guessidx = 0
+      }
+    } else {
+      messageLabel.text = "Wrong!"
+      guessidx = 0
+      let queue = dispatch_get_global_queue(QOS_CLASS_USER_INITIATED, 0)
+      dispatch_async(queue) {
+        // blink all buttons and restart game
+        dispatch_async(dispatch_get_main_queue()) {
+          for btn in self.buttons {
+            btn.highlighted = true
+          }
+        }
+        NSThread.sleepForTimeInterval(3.0)
+        dispatch_async(dispatch_get_main_queue()) {
+          for btn in self.buttons {
+            btn.highlighted = false
+          }
+        }
+        NSThread.sleepForTimeInterval(1.0)
+        dispatch_async(dispatch_get_main_queue()) {
+          self.flashSequence()
+        }
+      }
+    }
+  }
+  
+  @IBAction func startGame(sender: AnyObject) {
+    guessidx = 0
+    flashSequence()
+  }
+}
+
